c4406a8ae50c80e6e185fc31b80d10eb06b30807
Pierce Paynter <paynterpierce@gmail.com>
2020-06-28 23:51:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,34 @@ function nestedTarget(){
   return document.querySelector('#nested .target');
 }
 
-
+/*
 function increaseRankBy(n){
  const list = document.querySelectAll('.ranked-list');
  for (let i = 0; i < list.length; i++){
    list[i].innerHTML = list[i].innerHTML + parseInt(n);
  }
+}
+*/
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  const firstList = rankedLists[0]
+  const secondList = rankedLists[1]
+
+  let children = firstList.children
+  let start = 1
+
+  expect(children.length).to.equal(2);
+  for (let i = 0, l = children.length; i < l; i++) {
+    expect(parseInt(children[i].innerHTML)).to.equal(start + i + 3)
+  }
+
+  children = secondList.children
+  start = 12
+
+  expect(children.length).to.equal(3);
+  for (let i = 0, l = children.length; i < l; i++) {
+    expect(parseInt(children[i].innerHTML)).to.equal(start - i + 3)
+  }
 }
 
 function deepestChild(){
