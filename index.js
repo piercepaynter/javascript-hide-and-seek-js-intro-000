1bac2645748d69f00aa7d4d0ad7a8a50751eb1ba
Pierce Paynter <paynterpierce@gmail.com>
2020-06-28 23:19:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function nestedTarget(){
   return document.querySelector('#nested .target')
 }
 
-/*
-function increaseRankBy(n){
 
+function increaseRankBy(n){
+ return document.querySelector('#grand-node div div div div')
 }
 
 function deepestChild(){
 
 }
-*/
+
