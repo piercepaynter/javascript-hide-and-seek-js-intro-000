9bb562ec70e476ff0954bf7977fd4a1735429bf5
Pierce Paynter <paynterpierce@gmail.com>
2020-06-29 00:16:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget(){
+  return document.querySelector('#nested.target');
+}
+
+/*
+function nestedTarget(){
   var container = document.querySelector('#nested');
   return container.querySelector('.target');
 }
+*/
 
 function increaseRankBy(n){
  const list = document.querySelectorAll('.ranked-list');
