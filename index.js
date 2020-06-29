48a9570e484e7d33d424be420d87fa0070bc3cdc
Pierce Paynter <paynterpierce@gmail.com>
2020-06-29 00:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
- const list = document.querySelectorAll('.ranked-list');
+ const list = document.querySelectorAll('ul.ranked-list');
 
  for (let i = 0; i < list.length; i++){
    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
