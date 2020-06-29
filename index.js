ff8f3e570ade961410a4029a0e01287b65f4439c
Pierce Paynter <paynterpierce@gmail.com>
2020-06-29 00:29:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+ const list = document.querySelectorAll('ul.ranked-list li');
+
+ for (let i = 0; i < list.length; i++){
+   list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+ }
+}
+
+function deepestChild(){
+  var container = document.querySelector('#grand-node');
+  var children = container.querySelectorAll('div');
+
+  return children[children.length-1];
+}
