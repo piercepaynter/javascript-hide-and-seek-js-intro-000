e7dd9b5c8cb95ee7d533b6b8ab5521be8ccfcf23
Pierce Paynter <paynterpierce@gmail.com>
2020-06-29 00:18:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,30 +14,6 @@ function increaseRankBy(n){
  }
 }
 
-/*
-function increaseRankBy(n){
-  const rankedLists = document.querySelectorAll('.ranked-list')
-  const firstList = rankedLists[0]
-  const secondList = rankedLists[1]
-
-  let children = firstList.children
-  let start = 1
-
-  expect(children.length).to.equal(2);
-  for (let i = 0, l = children.length; i < l; i++) {
-    parseInt(children[i].innerHTML) = start + i + parseInt(n);
-  }
-
-  children = secondList.children
-  start = 12
-
-  expect(children.length).to.equal(3);
-  for (let i = 0, l = children.length; i < l; i++) {
-    parseInt(children[i].innerHTML) = start + i + parseInt(n);
-  }
-}
-*/
-
 function deepestChild(){
   return document.querySelector('#grand-node div div div div');
 }
